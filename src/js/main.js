963fc6fd21569f8959e2e4d8175619a5292e72b4
DeepakShinde123 <Deepakshinde123@proton.me>
2024-10-16 06:58:27 +0000

    local storage functionalty

--- src/js/main.js
+++ src/js/main.js
@@ -6,29 +6,63 @@ import Option from "./components/Option";
 const BASE_URL = `https://dog.ceo/api/`;
 
 const breedListEl = document.querySelector("#data-breed-list");
+console.log(breedListEl);
+
 const imageEl = document.querySelector("img");
 
-function getDogsList() {
-  return fetch(`${BASE_URL}breeds/list/all`)
-    .then((res) => res.json())
-    .then((data) => data.message)
-    .catch((err) => console.error("error aagyi", err));
+async function getDogsList() {
+  let breeds = JSON.parse(localStorage.getItem("breeds"));
+
+  if (!breeds) {
+    try {
+      const res = await fetch(`${BASE_URL}breeds/list/all`);
+      const data = await res.json();
+      localStorage.setItem("breeds", JSON.stringify(data.message));
+      breeds = data.message;
+    } catch (err) {
+      console.error("Error occured", err);
+    }
+  }
+
+  return breeds;
 }
 
-function getDogImage(breed) {
-  fetch(`${BASE_URL}breed/${breed}/images/random`)
-    .then((res) => res.json())
-    .then((data) => data.message);
+async function getDogImage(breed) {
+  try {
+    const res = await fetch(`${BASE_URL}breed/${breed}/images/random`);
+    const data = await res.json();
+    return data.message;
+  } catch (error) {
+    return console.error(error);
+  }
 }
 
 function renderSelect() {
   getDogsList().then((breedList) => {
+    const fragment = document.createDocumentFragment();
     for (let breed in breedList) {
       breedListEl.appendChild(Option(breed));
     }
   });
 }
 
-renderSelect();
+// async function renderSelect() {
+//   const dogsList = await getDogsList();
+//   Object.keys(dogsList).forEach((dogName) => {
+//     breedListEl.apppendChild(Option(dogName));
+//   });
+// }
 
-function renderImage() {}
+async function renderImage(breed) {
+  imageEl.src = "loading-thinking.gif";
+  const dogsImage = await getDogImage(breed);
+  imageEl.src = dogsImage;
+  imageEl.alt = breed;
+}
+
+breedListEl.addEventListener("change", async (e) => {
+  const currentValue = e.target.value;
+  renderImage(currentValue);
+});
+
+renderSelect();
